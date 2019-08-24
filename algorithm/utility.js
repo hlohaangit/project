49867ff3ceaa7ff227b6b7d3636d13d65d290f09
hlohaangit <hlohaan@gmail.com>
2019-08-24 17:43:22 +0530

    incomplete merge algo

--- algorithm/utility.js
+++ algorithm/utility.js
@@ -69,7 +69,42 @@ module.exports={
         }
     },
 
-    
+    merge (arr, t){
+        var nl=t;
+        var nr=arr.length-k
+        var l[nl];
+        var r[nr];
+        var i=0;
+        var j=0;
+        var k=0;
+        for (i=0;i<nl;++i){
+            l[i]=arr[i];
+        }
+        for (j=0;i<nr;++j){
+            l[j]=arr[k+j];
+        }
+        i=0;
+        j=0;
+        k=o;
+
+        while (i<nl&&j<nr){
+            if (l[i]<r[j]){
+                arr[k++]=l[i++];
+            }
+            else {
+                arr[k++]=r[j++];
+            }
+        }
+
+        while (j<nr){
+            arr[k++]=r[j++];
+        }
+        while (i<nl){
+            arr[k++]=l[i++];
+        }
+    }
+
+
 
 
 }
